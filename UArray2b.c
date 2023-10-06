bb9126c862aec05feb76442fad1c0ac2820bafd5
coopergolemme <coopergolemme@gmail.com>
2023-10-06 12:47:14 -0400

    commnets

--- UArray2b.c
+++ UArray2b.c
@@ -22,30 +22,40 @@ struct T {
 void check_and_print(int i, int j, UArray2b_T a, void *p1, void *p2);
 void fill_test(int i, int j, UArray2b_T a, void *p1, void *p2);
 
-int main (int argc, char **argv)
-{
-    (void) argc; (void) argv;
+// int main (int argc, char **argv)
+// {
+//     (void) argc; (void) argv;
 
-    int width = 9;
-    int height = 9;
-    int size = sizeof(int);
-    int blocksize = 2;
-    T test_array = UArray2b_new(width, height, size, blocksize);
+//     int width = 9;
+//     int height = 9;
+//     int size = sizeof(int);
+//     int blocksize = 2;
+//     T test_array = UArray2b_new(width, height, size, blocksize);
 
-    printf("test array width: %i\n", UArray2b_width(test_array));
-    printf("test array height: %i\n", UArray2b_height(test_array));
-    printf("test array size: %i\n", UArray2b_size(test_array));
-    printf("test array blocksize: %i\n", UArray2b_blocksize(test_array));
-    int x = 0;
-    UArray2b_map(test_array, fill_test, &x);
-    printf("\nNumber of maps: %d\n", x);
-    UArray2b_map(test_array, check_and_print, NULL);
+//     printf("test array width: %i\n", UArray2b_width(test_array));
+//     printf("test array height: %i\n", UArray2b_height(test_array));
+//     printf("test array size: %i\n", UArray2b_size(test_array));
+//     printf("test array blocksize: %i\n", UArray2b_blocksize(test_array));
+//     int x = 0;
+//     UArray2b_map(test_array, fill_test, &x);
+//     printf("\nNumber of maps: %d\n", x);
+//     UArray2b_map(test_array, check_and_print, NULL);
 
-    UArray2b_free(&test_array); 
+//     UArray2b_free(&test_array); 
 
-    return 0;
-}
+//     return 0;
+// }
 
+/********** UArray2b_new ********
+ *
+ * Purpose:
+ * Inputs: 
+ *
+ * Return: 
+ *
+ * Notes: 
+ * 
+ ************************/
 T UArray2b_new(int width, int height, int size, int blocksize)
 {
     assert(width >= 0 && height >= 0); // ??
@@ -58,7 +68,7 @@ T UArray2b_new(int width, int height, int size, int blocksize)
     uarray2b->blocksize = blocksize;
 
     int uarray2_dim_width = width / blocksize;
-    if (width %blocksize != 0){
+    if (width % blocksize != 0){
         uarray2_dim_width++;
     }
     int uarray2_dim_height = height / blocksize;
@@ -77,67 +87,154 @@ T UArray2b_new(int width, int height, int size, int blocksize)
             *elem = UArray_new(blocksize * blocksize, size);
         }
     }
-    printf(" block-width: %d\n", uarray2_dim_width);
-    printf(" block-height: %d\n", uarray2_dim_height);
-    
+        
     return uarray2b;
 }
 
 /* new blocked 2d array: blocksize as large as possible provided
  * block occupies at most 64KB (if possible)
  */
+
+ /********** function name ********
+ *
+ * Purpose:
+ * 
+ * Inputs: 
+ *	    
+ * Return: 
+ *
+ * Notes: 
+ * 
+ ************************/
 T UArray2b_new_64K_block(int width, int height, int size)
 {
     int x = ceil(sqrt(64 * 1024));
     return UArray2b_new(width, height, size, x);
 }
 
+/********** UArray2b_free ********
+ *
+ * Purpose:
+ * 
+ * Inputs: 
+ *	    
+ * Return: 
+ *
+ * Notes: 
+ * 
+ ************************/
 void UArray2b_free(T *array2b)
 {
     assert(array2b);
+    
     for (int i = 0; i < UArray2_height((*array2b)->elems); i++) {
         for (int j = 0; j < UArray2_width((*array2b)->elems); j++) {
             UArray_free(UArray2_at((*array2b)->elems, j, i));
         }
     }
+    
     UArray2_free(&((*array2b)->elems));
     FREE(*array2b);
 }
 
+/********** UArray2b_width ********
+ *
+ * Purpose:
+ * 
+ * Inputs: 
+ *	    
+ * Return: 
+ *
+ * Notes: 
+ * 
+ ************************/
 int UArray2b_width(T array2b)
 {
     assert(array2b);
     return array2b->width;
 
 }
+
+/********** UArray2b_height ********
+ *
+ * Purpose:
+ * 
+ * Inputs: 
+ *	    
+ * Return: 
+ *
+ * Notes: 
+ * 
+ ************************/
 int UArray2b_height(T array2b)
 {
     assert(array2b);
     return array2b->height;
 
 }
+
+/********** UArray2b_size ********
+ *
+ * Purpose:
+ * 
+ * Inputs: 
+ *	    
+ * Return: 
+ *
+ * Notes: 
+ * 
+ ************************/
 int UArray2b_size(T array2b)
 {
     assert(array2b);
     return array2b->size;
 
 }
+
+/********** UArray2b_blocksize ********
+ *
+ * Purpose:
+ * 
+ * Inputs: 
+ *	    
+ * Return: 
+ *
+ * Notes: 
+ * 
+ ************************/
 int UArray2b_blocksize(T array2b)
 {
     assert(array2b);
     return array2b->blocksize;
 }
 
+/********** flattened_index ********
+ *
+ * Purpose:
+ * 
+ * Inputs: 
+ *	    
+ * Return: 
+ *
+ * Notes: 
+ * 
+ ************************/
 int flattened_index(int col, int row, int width){
     return width * row + col;
 }
 
 
-
-
-/* return a pointer to the cell in the given column and row.
- * index out of range is a checked run-time error
- */
+ /********** Block_at ********
+ *
+ * Purpose:
+ * 
+ * Inputs: 
+ *	    
+ * Return: 
+ *
+ * Notes: 
+ * 
+ ************************/
 void *Block_at(T array2b, int column, int row)
 {
     assert(array2b);
@@ -151,6 +248,17 @@ void *Block_at(T array2b, int column, int row)
     return block;
 }
 
+/********** UArray2b_at ********
+ *
+ * Purpose:
+ * 
+ * Inputs: 
+ *	    
+ * Return: 
+ *
+ * Notes: 
+ * 
+ ************************/
 void *UArray2b_at(T array2b, int column, int row)
 {
     int bs = array2b->blocksize;
@@ -166,11 +274,17 @@ void *UArray2b_at(T array2b, int column, int row)
     return elem;
 }
 
-
-/*
-*
-*
-*/
+/********** UArray2b_map ********
+ *
+ * Purpose:
+ * 
+ * Inputs: 
+ *	    
+ * Return: 
+ *
+ * Notes: 
+ * 
+ ************************/
 extern void  UArray2b_map(T array2b, 
                           void apply(int col, int row, T array2b,
                                      void *elem, void *cl), 
@@ -178,13 +292,13 @@ extern void  UArray2b_map(T array2b,
 {
     int num_blocks_width = UArray2_width(array2b->elems);
     int num_blocks_height = UArray2_height(array2b->elems);
-
     int blocksize = array2b->blocksize;
+
     for (int row = 0; row < num_blocks_height; row++) {
         for (int col = 0; col < num_blocks_width; col++) {
 
             UArray_T *block = Block_at(array2b, col, row);
-            printf("NEW BLOCK at [col, row] : [%d, %d]\n", col, row);
+            // printf("NEW BLOCK at [col, row] : [%d, %d]\n", col, row);
             for(int block_col = 0; block_col < blocksize; block_col++) {
                 for (int block_row = 0; block_row < blocksize; block_row++) {
                     
@@ -209,12 +323,34 @@ extern void  UArray2b_map(T array2b,
     }
 }
 
+/********** check_and_print ********
+ *
+ * Purpose:
+ * 
+ * Inputs: 
+ *	    
+ * Return: 
+ *
+ * Notes: 
+ * 
+ ************************/
 void check_and_print(int col, int row, UArray2b_T a, void *elem, void *cl) 
 {
     (void) a; (void) cl;
     printf("ar[%d,%d]: %d\n", col, row, *(int*)elem);
 }
 
+/********** fill_test ********
+ *
+ * Purpose:
+ * 
+ * Inputs: 
+ *	    
+ * Return: 
+ *
+ * Notes: 
+ * 
+ ************************/
 void fill_test(int i, int j, UArray2b_T a, void *elem, void *cl) 
 {
     (void) a; (void) i; (void) j;
